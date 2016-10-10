68b540b5dbcbed51bd0c8c5accc161f41ff60af1
Bryan <bmart847@gmail.com>
2016-10-10 13:25:11 -0400

    Added variables and comments

--- src/main.c
+++ src/main.c
@@ -1,16 +1,28 @@
 #include "shellFunctions.h"
+#include "fatSupport.h"
 
 /* Main Function (Currently Just Runs Shell) */
 int main(int argc, char *argv[]) {
-	char *input;
-	char **cmd;
-	int status;
+	/* Declare Variables */
+	char *input, **cmd;
+	unsigned char* boot;            // Boot Sector
+	int mostSignificantBits, leastSignificantBits, bytesPerSector, status;
 
+	/* Main Shell Logic */
 	do {
+		/* Shows the prompt */
 		printf("> ");
+
+		/* Grab input */
 		input = get_line();
+
+		/* Parse input */
 		cmd = parseInput(input);
+
+		/* Execute input */
 		status = cmdExecute(cmd);
+
+		/* Free memory */
 		free(input);
 		free(cmd);
 	} while (status == 0);
