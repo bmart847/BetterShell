1c539904c014ce83f1d0a5fca759f8cde16068d3
Bryan <bmart847@gmail.com>
2016-10-10 17:21:48 -0400

    Comments

--- src/pfe.c
+++ src/pfe.c
@@ -1,49 +1,59 @@
-int checkRange(int x, int y){
-	if(x>y){
+/* PFE Command Functions */
+
+int checkRange(int x, int y) {	/* Verifies that the provided integers are acceptable */
+
+	/* Conditions */
+	if(x>y){	/* Starting Entry MUST be less than Ending Entry */
 		printf("Error: x > y\n");
 		return 1;
-	} else if(x<2){
+	} else if(x<2){	/* Starting entry must be at least 2 */
 		printf("Error: x < 2\n");
 		return 1;
-	} else {
+	} else { 	/* Parameters are OK */
 		return 0;
 	}
 
 }
 
-unsigned char* readFAT12Table(int fatTable) {
+unsigned char* readFAT12Table(int fatTable) {	/* Reads FAT12 Table and returns either Table 1 or Table 2 */
+	/* Declare Variables */
 	unsigned char* fat;
+	int i, br=0;
 
+	/* Allocate Memory */
 	fat = (unsigned char*) malloc(9 * (BYTES_PER_SECTOR * sizeof(unsigned char)));
 
-	int i, br = 0;
-	if(fatTable == 1) {
+	/* Conditions */
+	if(fatTable == 1) { /* Return 1st FAT */
 		for(i=1; i<10; i++){
 			br = br + read_sector(i, &fat[(i-1)*BYTES_PER_SECTOR]);
 		}
 		return fat;
-	} else if(fatTable == 2) {
+	} else if(fatTable == 2) { /* Return 2nd FAT */
 		for(i=10; i<19; i++){
 			br = br + read_sector(i, &fat[(i-10)*BYTES_PER_SECTOR]);
                 }
 		return fat;
-	} else {
+	} else { /* Invalid input */
 		free(fat);
 		return NULL;
 	}
 }
 
-
+/* Prints FAT12 Table Entries from (arg1+1) to arg2 */
 int pfe(int arg1, int arg2){
+	/* Declare Variables */
 	unsigned char *fat1 = readFAT12Table(1), *fat2 = readFAT12Table(2);
 	int i;
 
-	if(checkRange(arg1, arg2) == 1) { printf("checkRange Failed\n"); }
-
-	else {
+	/* Conditions */
+	if(checkRange(arg1, arg2) == 1) { /* Utilize checkRange function to verify arguments */
+		printf("Invalid parameter values.\n");
+	} else { /* If it checks out... */
         	for(i=(arg1+1); i <= arg2; i++) {
                 	printf("Fat Entry %i: %x\n", i, get_fat_entry(i, fat1));
         	}	
 	}
+
 	return(0);
 }
