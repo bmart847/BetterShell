b98b6ab7f59791f8300037fbcdec0cd83aedd5d5
Bryan <bmart847@gmail.com>
2016-10-11 15:26:38 -0400

    Working on reading root sectors

--- src/libs/root.c
+++ src/libs/root.c
@@ -1,3 +1,14 @@
 unsigned char* readRootSectors() {
+	/* Declare Variables */
+ 	unsigned char* root;
+	int i, br;
 
+	/* Allocate Space */
+	root = (unsigned char*) malloc(14 * (BYTES_PER_SECTOR * sizeof(unsigned char)));
+
+	for(i=19;i<33;i++) {
+		br = br + read_sector(i, &root[(i-19)*BYTES_PER_SECTOR];
+	}
 }
+
+
