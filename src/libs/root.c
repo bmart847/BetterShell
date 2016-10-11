539e3a41cafcaad93ce0278127675a515563c7bf
Bryan <bmart847@gmail.com>
2016-10-11 16:40:55 -0400

    Few changes.

--- src/libs/root.c
+++ src/libs/root.c
@@ -7,8 +7,10 @@ unsigned char* readRootSectors() {
 	root = (unsigned char*) malloc(14 * (BYTES_PER_SECTOR * sizeof(unsigned char)));
 
 	for(i=19;i<33;i++) {
-		br = br + read_sector(i, &root[(i-19)*BYTES_PER_SECTOR];
+		br = br + read_sector(i, &root[(i-19)*BYTES_PER_SECTOR]);
 	}
+
+	return root;
 }
 
 
