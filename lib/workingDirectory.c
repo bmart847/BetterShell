4cc3912972cc7b6660841db96463e5c503912d85
Bryan <bmart847@gmail.com>
2016-11-02 23:02:10 -0400

    Fixed path to remove trailing '/' and added some manipulation of new variables

--- lib/workingDirectory.c
+++ lib/workingDirectory.c
@@ -3,7 +3,7 @@
 // Initializes the shared memory space used to store the working directory
 void initWorkingDir() {
 	workingDir *init;
-	shm_id = shmget(IPC_PRIVATE, 200*sizeof(char*), IPC_CREAT | 0666);
+	shm_id = shmget(IPC_PRIVATE, 250*sizeof(char*), IPC_CREAT | 0666);
 	if(shm_id < 0) {
 		printf("shmget Error\n");
 		exit(1);
@@ -13,31 +13,42 @@ void initWorkingDir() {
 	init->wdPath[0] = '/';
 	init->wdPath[1] = '\0';
 	init->wdSize = 1;
+	init->wdOffset = 1;
+	init->wdFLC = 0;
+	shmdt(init);
 }
 
 void dirAdd(char* new) {
 	workingDir* change;
-	unsigned int newSize = 1, i;
+	unsigned int newSize = 0, i;
 
 	for(i = 0; new[i] != '\0'; i++){}
 	newSize = newSize + i;
 
 	change = shmat(shm_id, 0, 0);
+	if(change->wdSize != 1) {
+		strcat(change->wdPath, "/\0");
+		newSize++;
+	}
+
 	strcat(change->wdPath, new);
-	strcat(change->wdPath, "/\0");
+	change->wdOffset = change->wdSize;
 	change->wdSize = change->wdSize + newSize;
+	shmdt(change);
 }
 
 void dirRemove() {
 	workingDir* change = shmat(shm_id, 0, 0);
-	int index = change->wdSize - 1, removeSize = 0;
+	int index = change->wdSize - 1, removeSize = 1;
 	change->wdPath[index] = '\0';
 	while(change->wdPath[index] != '/') {
 		change->wdPath[index] = '\0';
 		removeSize++;
 		index--;
 	}
+	change->wdPath[index] = '\0';
 	change->wdSize = change->wdSize - removeSize;
+	shmdt(change);
 }
 
 // Stub
