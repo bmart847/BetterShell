f2c7e056251564199ddafa6cd3a1a34ea3f156f5
Bryan <bmart847@gmail.com>
2016-11-02 20:39:53 -0400

    A few changes and added a stub for use during testing

--- lib/workingDirectory.c
+++ lib/workingDirectory.c
@@ -2,10 +2,21 @@
 
 // Initializes the shared memory space used to store the working directory
 void initWorkingDir() {
+	workingDir *init;
 	shm_id = shmget(IPC_PRIVATE, 200*sizeof(char*), IPC_CREAT | 0666);
 	if(shm_id < 0) {
 		printf("shmget Error\n");
 		exit(1);
 	}
 	printf("Shared memory initialized!\n");
+	init = shmat(shm_id, 0, 0);
+	init->wd = "/";
+}
+
+// Stub
+int main(){
+	initWorkingDir();
+	workingDir *test = shmat(shm_id, 0, SHM_RDONLY);
+	printf("%s\n", test->wd);
+	return;
 }
--- lib/workingDirectory.h
+++ lib/workingDirectory.h
@@ -1,7 +1,12 @@
 #include "sys/shm.h"
 #include "sys/ipc.h"
+#include "stdio.h"
 
 int shm_id;
+
+typedef struct {
+	char* wd;
+} workingDir;
 
 void initWorkingDir();
 
