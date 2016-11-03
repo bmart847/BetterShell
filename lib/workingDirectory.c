4186581bb7483c0b657ab89f1e4755870f959813
Joe L <jlistro@gmail.com>
2016-11-03 03:15:41 -0400

    Fix merge issues

--- lib/workingDirectory.c
+++ lib/workingDirectory.c
@@ -2,22 +2,9 @@
 
 // Initializes the shared memory space used to store the working directory
 void initWorkingDir() {
-<<<<<<< HEAD
-    workingDir *init;
-    shm_id = shmget(IPC_PRIVATE, 200*sizeof(char*), IPC_CREAT | 0666);
-    if(shm_id < 0) {
-        printf("shmget Error\n");
-        exit(1);
-    }
-    printf("Shared memory initialized!\n");
-    init = shmat(shm_id, 0, 0);
-    init->wdPath[0] = '/';
-    init->wdPath[1] = '\0';
-    init->wdSize = 1;
-=======
 	workingDir *init;
-	shm_id = shmget(IPC_PRIVATE, 250*sizeof(char*), IPC_CREAT | 0666);
-	if(shm_id < 0) {
+	shm_id = shmget(IPC_PRIVATE, 250 * sizeof(char*), IPC_CREAT | 0666);
+	if (shm_id < 0) {
 		printf("shmget Error\n");
 		exit(1);
 	}
@@ -28,43 +15,18 @@ void initWorkingDir() {
 	init->wdSize = 1;
 	init->wdOffset = 1;
 	shmdt(init);
->>>>>>> refs/remotes/origin/master
 }
 
 // Adds directory 'new' to the end of the working directory
 void dirAdd(char* new) {
-<<<<<<< HEAD
-    workingDir* change;
-    unsigned int newSize = 1, i;
-    
-    for(i = 0; new[i] != '\0'; i++){}
-    newSize = newSize + i;
-    
-    change = shmat(shm_id, 0, 0);
-    strcat(change->wdPath, new);
-    strcat(change->wdPath, "/\0");
-    change->wdSize = change->wdSize + newSize;
-}
-
-void dirRemove() {
-    workingDir* change = shmat(shm_id, 0, 0);
-    int index = change->wdSize - 1, removeSize = 0;
-    change->wdPath[index] = '\0';
-    while(change->wdPath[index] != '/') {
-        change->wdPath[index] = '\0';
-        removeSize++;
-        index--;
-    }
-    change->wdSize = change->wdSize - removeSize;
-=======
 	workingDir* change;
 	unsigned int newSize = 0, i;
 
-	for(i = 0; new[i] != '\0'; i++){}
+	for (i = 0; new[i] != '\0'; i++) {}
 	newSize = newSize + i;
 
 	change = shmat(shm_id, 0, 0);
-	if(change->wdSize != 1) {
+	if (change->wdSize != 1) {
 		strcat(change->wdPath, "/\0");
 		newSize++;
 	}
@@ -73,14 +35,13 @@ void dirRemove() {
 	change->wdOffset = change->wdSize + 2;
 	change->wdSize = change->wdSize + newSize;
 	shmdt(change);
->>>>>>> refs/remotes/origin/master
 }
 // Removes one directory from the end of the working directory (for 'cd ..')
 void dirRemove() {
 	workingDir* change = shmat(shm_id, 0, 0);
 	int index = change->wdSize - 1, removeSize = 1;
 	change->wdPath[index] = '\0';
-	while(change->wdPath[index] != '/') {
+	while (change->wdPath[index] != '/') {
 		change->wdPath[index] = '\0';
 		removeSize++;
 		index--;
@@ -88,57 +49,32 @@ void dirRemove() {
 	change->wdPath[index] = '\0';
 	change->wdSize = change->wdSize - removeSize;
 
-        change->wdOffset = change->wdSize;
-        do {
-            	change->wdOffset--;
-        } while(change->wdPath[change->wdOffset-2] != '/');
+	change->wdOffset = change->wdSize;
+	do {
+		change->wdOffset--;
+	} while (change->wdPath[change->wdOffset - 2] != '/');
 
-<<<<<<< HEAD
-// Stub
-int main(){
-    workingDir* test;
-    initWorkingDir();
-    test = shmat(shm_id, 0, SHM_RDONLY);
-    printf("%s - %i\n", test->wdPath, test->wdSize);
-    shmdt(test);
-    
-    dirAdd("New");
-    dirAdd("Folder");
-    dirAdd("Path");
-    
-    test = shmat(shm_id, 0, SHM_RDONLY);
-    printf("%s - %i\n", test->wdPath, test->wdSize);
-    shmdt(test);
-    
-    dirRemove();
-    dirRemove();
-    
-    test = shmat(shm_id, 0, SHM_RDONLY);
-    printf("%s - %i\n", test->wdPath, test->wdSize);
-    shmdt(test);
-    return;
-=======
 	shmdt(change);
 }
 
 // Sets the working directory to the given string
 void dirSet(char* newPath) {
-        workingDir* change = shmat(shm_id, 0, 0);
-        unsigned int newSize = 0, oldSize = change->wdSize, i;
+	workingDir* change = shmat(shm_id, 0, 0);
+	unsigned int newSize = 0, oldSize = change->wdSize, i;
 
 	memset(&(change->wdPath), '\0', sizeof(change->wdPath));
 
 	change->wdDepth = 0;
-        for(newSize = 0; newPath[newSize] != '\0'; newSize++) {
-		if(newPath[newSize] == '/') {
+	for (newSize = 0; newPath[newSize] != '\0'; newSize++) {
+		if (newPath[newSize] == '/') {
 			change->wdDepth++;
 			change->wdOffset = newSize + 2;
 		}
 	}
 
-        strcat(change->wdPath, newPath);
-        change->wdSize = newSize;
-        shmdt(change);
+	strcat(change->wdPath, newPath);
+	change->wdSize = newSize;
+	shmdt(change);
 }
 
 char* dirGet() {
@@ -146,10 +82,9 @@ char* dirGet() {
 	int i;
 	char* tmp = malloc(access->wdSize * sizeof(char*));
 
-	for(i = 0; i < access->wdSize; i++) {
+	for (i = 0; i < access->wdSize; i++) {
 		tmp[i] = access->wdPath[i];
 	}
 	shmdt(access);
 	return tmp;
->>>>>>> refs/remotes/origin/master
 }
--- lib/workingDirectory.h
+++ lib/workingDirectory.h
@@ -7,23 +7,14 @@
 int shm_id;
 
 typedef struct {
-<<<<<<< HEAD
-	char wd[200];
-    unsigned int wdSize;
-=======
 	char wdPath[200];
 	unsigned int wdDepth;
 	unsigned int wdSize;
 	unsigned int wdOffset;
->>>>>>> refs/remotes/origin/master
 } workingDir;
 
 void initWorkingDir();
 void dirAdd(char*);
-<<<<<<< HEAD
-void dirRemove();
-=======
 void dirRemove();
 void dirSet(char*);
 char* dirGet();
->>>>>>> refs/remotes/origin/master
