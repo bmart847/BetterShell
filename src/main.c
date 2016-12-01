c97c18917b0e9d7d92a047651ea195a7098730e8
Bryan Martin <bmart847@gmail.com>
2016-12-01 03:55:38 -0500

    Added argv so path can be specified as argument

--- src/main.c
+++ src/main.c
@@ -33,7 +33,7 @@ int main(int argc, char *argv[]) {
 	int mostSignificantBits, leastSignificantBits, bytesPerSector, status;
 
 	/* File to open, later allow it to be specified */
-	FILE_SYSTEM_ID = fopen("test/floppy3", "r+");
+	FILE_SYSTEM_ID = fopen(argv[1], "r+");
 
 	/* Open file */
 	if (FILE_SYSTEM_ID == NULL) {
@@ -43,6 +43,7 @@ int main(int argc, char *argv[]) {
 
 	/* Read Boot Sector */
 	boot = (unsigned char*) malloc(BYTES_PER_SECTOR * sizeof(unsigned char));
+	
 	if(read_sector(0,boot) == -1) {
 		printf("Error reading the boot sector.\n");
 	}
