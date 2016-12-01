40eabe804a45da860a0e7fb08801a59355f4c9fd
joeListro <jlistro@gmail.com>
2016-12-01 05:48:24 -0500

    Removed deprecated functions

--- src/helper/fat.h
+++ src/helper/fat.h
@@ -57,9 +57,7 @@ typedef struct
 extern char* WORKING_DIRECTORY;
 extern int BYTES_PER_SECTOR;
 
-void enterDir(char *dirName);
 short existingDirectory(char* path);
 short searchForDir(short curFLC, char* target);
 char* getEntryName(dirEntry directory);
-void listDirectory();
 #endif
