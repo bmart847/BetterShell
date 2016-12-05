641e526b90183a76a13dd094b0dac836b5f2fa00
joeListro <jlistro@gmail.com>
2016-12-04 22:49:46 -0500

    Dat coding standard

--- src/helper/shellFunctions.c
+++ src/helper/shellFunctions.c
@@ -6,12 +6,14 @@
 extern const key_t SHM_KEY;
 
 // Initializes the shared memory space used to store the working directory
-void initWorkingDir(char *filename) {
+void initWorkingDir(char *filename)
+{
 	sharedMemory *init;
 	shm_id = shmget(SHM_KEY, 250 * sizeof(char*), IPC_CREAT | 0666);
 	unsigned int i;
 
-	if (shm_id < 0) {
+	if (shm_id < 0)
+	{
 		printf("shmget Error\n");
 		exit(1);
 	}
@@ -21,7 +23,8 @@ void initWorkingDir(char *filename) {
 	init->wdPath[1] = '\0';
 	init->wdSize = 1;
 
-	for (i = 0; filename[i] != '\0'; i++) {
+	for (i = 0; filename[i] != '\0'; i++)
+	{
 		init->fName[i] = filename[i];
 	}
 	
@@ -31,13 +34,15 @@ void initWorkingDir(char *filename) {
 }
 
 /* Obtains input from the user */
-char *get_line() {
+char *get_line()
+{
 	/* Initialize Variables */
 	char *input = NULL;
 	ssize_t bufsize = 0;
 
 	/* Get the line */
-	if (getline(&input, &bufsize, stdin) == -1){
+	if (getline(&input, &bufsize, stdin) == -1)
+	{
 		perror("Error_Getting_Line");
 	}
 
@@ -46,7 +51,8 @@ char *get_line() {
 }
 
 /* Parses the user input */
-char **parseInput(char *input) {
+char **parseInput(char *input)
+{
 	/* Initialize Variables */
 	int pos = 0;
 	char **letters = malloc(128);
@@ -55,7 +61,8 @@ char **parseInput(char *input) {
 	/* Traverse Input */
 	letter = strtok(input, " \t\r\n\a");
 
-	while(letter != NULL) {
+	while(letter != NULL)
+	{
 		letters[pos] = letter;
 		pos ++;
 		letter = strtok(NULL, " \t\r\n\a");
@@ -69,7 +76,8 @@ char **parseInput(char *input) {
 }
 
 /* Forks a process for the given command */
-int cmdLaunch(char *input, char **args) {
+int cmdLaunch(char *input, char **args)
+{
 	/* Initialize Variables */
 	pid_t pid, wpid;
 	int stat;
@@ -78,17 +86,24 @@ int cmdLaunch(char *input, char **args) {
 	pid = fork();
 
 	/* Error Handlings */
-	if(pid == 0) {
-		if(execvp(input, args) == -1) {
+	if(pid == 0)
+	{
+		if(execvp(input, args) == -1)
+		{
 			perror("Error_Launching_Process");
 		}
 		return 1;
-	} else if (pid < 0) {
+	}
+	else if (pid < 0)
+	{
 		perror("Error_Launching_Process");
 		return 2;
-	} else {
+	}
+	else
+	{
 		/* Wait for child process to complete */
-		do {
+		do 
+		{
 			wpid = waitpid(pid, &stat, WUNTRACED);
 		} while (!WIFEXITED(stat) && !WIFSIGNALED(stat));
 
@@ -98,41 +113,69 @@ int cmdLaunch(char *input, char **args) {
 }
 
 /* Determines the command and performs neccessary actiions */
-int cmdExecute(char **input) {
+int cmdExecute(char **input)
+{
 	/* Initialize Variables */
 	char *leadCmd = input[0];
 	int index = 1;
 
 	/* Determine command and take action */
-	if(leadCmd == NULL) {
+	if(leadCmd == NULL)
+	{
 		return 0;
-	} else if (strcmp(leadCmd, "cat") == 0) {
+	}
+	else if (strcmp(leadCmd, "cat") == 0)
+	{
 		cmdLaunch("bin/cat", input);
-	} else if (strcmp(leadCmd, "cd") == 0) {
+	}
+	else if (strcmp(leadCmd, "cd") == 0)
+	{
 		cmdLaunch("bin/cd", input);
-	} else if (strcmp(leadCmd, "df") == 0) {
+	}
+	else if (strcmp(leadCmd, "df") == 0)
+	{
 		cmdLaunch("bin/df", input);
-	} else if (strcmp(leadCmd, "help") == 0) {
-        printf("To-Do\n");
-	} else if (strcmp(leadCmd, "ls") == 0) {
+	}
+	else if (strcmp(leadCmd, "help") == 0)
+	{
+        	printf("To-Do\n");
+	}
+	else if (strcmp(leadCmd, "ls") == 0)
+	{
 		cmdLaunch("bin/ls", input);
-	} else if (strcmp(leadCmd, "mkdir") == 0) {
+	}
+	else if (strcmp(leadCmd, "mkdir") == 0)
+	{
 		cmdLaunch("bin/mkdir", input);
-	} else if (strcmp(leadCmd, "pbs") == 0) {
+	}
+	else if (strcmp(leadCmd, "pbs") == 0)
+	{
 		cmdLaunch("bin/pbs", input);
-	} else if (strcmp(leadCmd, "pfe") == 0) {
+	}
+	else if (strcmp(leadCmd, "pfe") == 0)
+	{
 		cmdLaunch("bin/pfe", input);
-	} else if (strcmp(leadCmd, "pwd") == 0) {
-        cmdLaunch("bin/pwd", input);
-	} else if (strcmp(leadCmd, "rm") == 0) {
-        cmdLaunch("bin/rm", input);
-	} else if (strcmp(leadCmd, "rmdir") == 0) {
-        cmdLaunch("bin/rmdir", input);
-	} else if (strcmp(leadCmd, "touch") == 0) {
-        cmdLaunch("bin/touch", input);
-	} else if (strcmp(leadCmd, "exit") == 0) {
+	}
+	else if (strcmp(leadCmd, "pwd") == 0)
+	{
+		cmdLaunch("bin/pwd", input);
+	}
+	else if (strcmp(leadCmd, "rm") == 0)
+	{
+        	cmdLaunch("bin/rm", input);
+	}
+	else if (strcmp(leadCmd, "rmdir") == 0)
+	{
+        	cmdLaunch("bin/rmdir", input);
+	}
+	else if (strcmp(leadCmd, "touch") == 0)
+	{
+        	cmdLaunch("bin/touch", input);
+	}
+	else if ((strcmp(leadCmd, "exit") == 0) || (strcmp(leadCmd, "quit") == 0))
+	{
 		printf("Quitting\n");
-		return 1;
+		return; 
 	} else {
 		printf("Command not recognized\n");
 	}
