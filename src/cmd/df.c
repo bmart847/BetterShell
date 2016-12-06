5b82e762494e13be265004564002d6c16fd3c202
Bryan Martin <bmart847@gmail.com>
2016-12-06 00:45:31 -0500

    Added some basics to df

--- src/cmd/df.c
+++ src/cmd/df.c
@@ -24,7 +24,12 @@ int df_help();
 
 int main(int argc, char *argv[])
 {
-	// Just a stub
+	shm_id = shmget(SHM_KEY, 250 * sizeof(char*), 0666);
+	sharedMemory* share = shmat(shm_id, 0, 0);
+
+	char* filename = filenameGet();
+	FILE_SYSTEM_ID = fopen(filename, "r+");
+
 	printf("df has been called\n");
 	return 0;
 }
