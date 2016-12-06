f6e0e3fe1f4436343783c9349b9fa8a31fed0f49
joeListro <jlistro@gmail.com>
2016-12-06 11:06:20 -0500

    Use cd.c from master

--- src/cmd/cd.c
+++ src/cmd/cd.c
@@ -25,84 +25,30 @@ int cd_help();
 
 int main(int argc, char *argv[])
 {
-	/* Declare variables */
-	char* newPath;
-	short newFLC;
-
-	/* Access shared memory */
 	shm_id = shmget(SHM_KEY, 250 * sizeof(char*), 0666);
 	sharedMemory* share = shmat(shm_id, 0, 0);
 
 	char* filename = filenameGet();
 	FILE_SYSTEM_ID = fopen(filename, "r+");
-
+	
 	if (argc == 1)
 	{
-		/* Change to the home (root) directory */
+		// Change the working directory to root
 		dirSet("/");
 		filenameSet("");
 		flcSet(0);
-		return 0;
 	}
 	else if (argc == 2)
 	{
-		/* Correct number of arguments, cd to the specified directory */
-		if (strcmp(argv[1], "..") == 0)
-		{
-			dirRemove();
-			newFLC = existingDirectory(dirGet());
-			if (newFLC != -1)
-			{
-				flcSet(newFLC);
-			}
-			else
-			{
-				/* Not good, dirRemove() should stop at root */
-				printf("ERROR: CD failed\n");
-				return -1;
-			}
-		}
-		else if (strcmp(argv[1], "/") != 0)
-		{
-			/* If the specified path is relative, concatonate it onto the working directory. */
-			newPath = dirGet();
-			strcat(newPath, argv[1]);
-			newFLC = existingDirectory(newPath);
-			if (newFLC != -1)
-			{
-				flcSet(newFLC);
-				dirAdd(argv[1]);
-			}
-			else
-			{
-				/* Directory does not exist */
-				printf("Directory does not exist!\n");
-			}
-		}
-		else
-		{
-			/* Change to the specified absolute path */
-			newFLC = existingDirectory(argv[1]);
-			if (newFLC == -1)
-			{
-				/* Directory does not exist */
-				printf("Directory does not exist!\n");
-			}
-			else
-			{
-				flcSet(newFLC);
-				dirSet(argv[1]);
-			}
-		}
+		// CD to the given path name
+		dirAdd(argv[1]);
 	}
 	else
 	{
-		/* Wrong number of arguments */
+		// Too many args
 		cd_help();
 	}
-
-	printf("%s", dirGet());
-
+	
 	return 0;
 }
 
